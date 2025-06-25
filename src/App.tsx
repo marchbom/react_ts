c8bf53e3b83f12cff9a0d0315d49660f83698638
Bomin Kim <sirubom1203@gmail.com>
2025-06-25 23:12:26 +0900

    29강까지

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
-
 export default function App() {
+  const reArray = [1, 2, 3, 4];
+  const reObject = { name: "bomin", age: 20 };
+  const reFunction = () => "리턴값";
+  const reDate = new Date();
+  const reRegExp = /react/i;
+  const ReMap = new Map<string, string>([
+    ["key1", "value1"],
+    ["key2", "value2"],
+  ]);
+  const ReSet = new Set<number>([1, 2, 3, 4]);
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <h2>참조값</h2>
+      <p>배열 : {reArray}</p>
+      <p>객체 : {JSON.stringify(reObject)}</p>
+      <p>함수 : {reFunction.toString()}</p>
+      <p>Date: {reDate.toString()}</p>
+      <p>정규식 : {reRegExp.toString()}</p>
+      <p>Map: {JSON.stringify(Array.from(ReMap))}</p>
+      <p>Set : {JSON.stringify(Array.from(ReSet))}</p>
     </>
   );
 }
