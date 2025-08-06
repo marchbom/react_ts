d92581acf5fc7a4111531b59c70566a6683a14ee
Bomin Kim <sirubom1203@gmail.com>
2025-08-06 20:14:49 +0900

    70강 카운터 컴포넌트 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
+import Count from "./components/Count";
 
 export default function App() {
-  const [name, setName] = useState("jack");
-  const [age, setAge] = useState(20);
-  const [gender, setGender] = useState("male");
-  const stateHandler = () => {
-    setName("bomin");
-    setAge(20);
-    setGender("female");
-  };
   return (
     <>
-      <h1>name : {name}</h1>
-      <h1>age: {age}</h1>
-      <h1>gender: {gender}</h1>
-      <button onClick={stateHandler}>change state</button>
+      <Count />
     </>
   );
 }
---
+++ src/components/CountDisplay.tsx
@@ -0,0 +1,7 @@
+export default function CountDisplay({ count }: { count: number }) {
+  return (
+    <>
+      <h1>CountDisplay : {count}</h1>
+    </>
+  );
+}
