cea2814f4c729ecdab74ed2e4627772a5334468d
Bomin Kim <sirubom1203@gmail.com>
2025-08-06 18:14:34 +0900

    58강 상태 변경미션

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import MissionButton from "./components/ui/MissionButton";
+import { useState } from "react";
 
 export default function App() {
-  const clickHandler = (message: string) => {
-    alert(message);
+  const [name, setName] = useState("jack");
+  const [age, setAge] = useState(20);
+  const [gender, setGender] = useState("male");
+  const stateHandler = () => {
+    setName("bomin");
+    setAge(20);
+    setGender("female");
   };
   return (
     <>
-      <MissionButton clickHandler={clickHandler} message="미션 성공!">
-        미션 수행
-      </MissionButton>
+      <h1>name : {name}</h1>
+      <h1>age: {age}</h1>
+      <h1>gender: {gender}</h1>
+      <button onClick={stateHandler}>change state</button>
     </>
   );
 }
--- src/components/UserProfile2.tsx
+++
@@ -1,7 +0,0 @@
-export default function UserProfile2({ children }: { children: React.ReactElement }) {
-  return (
-    <>
-      <div>{children}</div>
-    </>
-  );
-}
